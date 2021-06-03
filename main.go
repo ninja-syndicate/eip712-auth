37e96ca86559a6477e1b4db1f6f48b25c861ebff
xenowits <kumar.abhishek111b@gmail.com>
2021-06-03 18:35:59 +0530

    feat: complete server side verification & jwt generation

--- main.go
+++ main.go
@@ -56,7 +56,6 @@ func RequestNonceHandler() func(w http.ResponseWriter, r *http.Request) (int, er
 
 func SignedMessageHandler() func(w http.ResponseWriter, r *http.Request) (int, error) {
 	fn := func(w http.ResponseWriter, r *http.Request) (int, error) {
-<<<<<<< HEAD
 		nonce := string(r.Header.Get("x-api-nonce"))
 		signature := string(r.Header.Get("x-api-signature"))
 		publicKey := string(r.Header.Get("x-api-publickey"))
@@ -86,9 +85,6 @@ func SignedMessageHandler() func(w http.ResponseWriter, r *http.Request) (int, e
 		}
 		json.NewEncoder(w).Encode(response)
 		return http.StatusAccepted, nil
-=======
-		return http.StatusNotImplemented, terror.ErrNotImplemented
->>>>>>> feat: add function to create nonce and signature
 	}
 	return fn
 }
